d422cdda9d1a3548fb8825de7d5e10a3ea244e45
TimmyChan99 <felemenoun@gmail.com>
2022-06-03 18:55:12 +0100

    setup linters

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::GreetingsController < ApplicationController
-	def index
+  def index
     @message = Greeting.find(Random.rand(1..5))
     render json: { messages: @message.message }.to_json
   end
