670c365a0ac85a5c428aee6fb4a3355b14c98a1c
TimmyChan99 <felemenoun@gmail.com>
2022-06-03 15:58:31 +0100

    Setup greetings controller

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::GreetingsController < ApplicationController
+	def index
+    @message = Message.find(Random.rand(1..5))
+    render json: { messages: @message.message }.to_json
+  end
+end
